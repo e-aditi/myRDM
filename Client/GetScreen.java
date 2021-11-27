d8676453b407c924db3e69916250b9398147fd54
e-aditi <aditieddula@gmail.com>
2021-11-27 10:06:28 +0530

    changed the cliend side code - getScreen to receive the image

--- Client/GetScreen.java
+++ Client/GetScreen.java
@@ -13,7 +13,7 @@ class GetScreen extends Thread{
 	private JPanel panel = null;
 	private boolean go = true;
 	InputStream is = null;
-	Image serverScreen = null;
+	//Image serverScreen = null;
 
 	public GetScreen(InputStream in,JPanel p){
 		is = in;
@@ -24,20 +24,20 @@ class GetScreen extends Thread{
 	public void run(){
 		try{
 			//Read screenshots of the client and then draw them
-			  byte[] sizeAr = new byte[4];
+			while(go) {
+				byte[] sizeAr = new byte[4];
 				is.read(sizeAr);
 				int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
 
 				byte[] imageAr = new byte[size];
 				is.read(imageAr);
 
-				serverScreen = serverScreen.getScaledInstance(panel.getWidth(),panel.getHeight(),Image.SCALE_FAST);
+				BufferedImage serverScreen = ImageIO.read(new ByteArrayInputStream(imageAr));
 
 				//Draw the received screenshots
 				Graphics graphics = panel.getGraphics();
 				graphics.drawImage(serverScreen, 0, 0, panel.getWidth(), panel.getHeight(), panel);
 			}
-
 		} catch(Exception e) {
 			System.out.println(e);
 		}
